28a41d97501d044954c9eb57286108f7e78a2bfb
Rodrigo <Rodrigo@HP_RODRIGO>
2023-09-17 23:01:21 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio02_lab4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio02_lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int exponente = 2;
+            int base1 = 5;
+            int resultado = 1;
+
+            for (int i = 0; i < exponente; i++)
+            {
+                resultado = resultado * base1;
+            }
+
+            Console.WriteLine("El resultado de la potencia es: " + resultado);
+            Console.ReadKey();
+        }
+    }
+}
